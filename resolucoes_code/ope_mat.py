22625395e5834eac00ca48f943498dac03262b28
Bea <166550950+S0L0B@users.noreply.github.com>
2026-04-24 09:37:07 -0300

    Add user input and sum operation in ope_mat.py
    colocando os codigos pedidos no exercicio

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,9 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Recebendo dois números do usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Realizando uma operação (soma)
+resultado = num1 + num2
+
+# Exibindo o resultado
+print("Resultado da soma:", resultado)
